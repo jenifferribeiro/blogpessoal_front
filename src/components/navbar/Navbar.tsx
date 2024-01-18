fdef8314fcbe4d3566a564d708d812b862713993
Jeniffer Ribeiro <js.ribei@hotmail.com>
2024-01-18 16:13:46 -0300

    React

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 
 
 function Navbar() {
  
+  const navigate = useNavigate();
   
+  const {handleLogout} = useContext(AuthContext) 
+
+  function logout(){
+
+    handleLogout()
+    alert('O usuário foi desconectado com sucesso!')
+    navigate('/login')
+  }
 
   return (
     <>
@@ -19,7 +30,7 @@ function Navbar() {
               <div className='hover:underline'>Temas</div>
               <div className='hover:underline'>Cadastrar tema</div>
               <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+              <Link to ='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
           </div>
         </div>
